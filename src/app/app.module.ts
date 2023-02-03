e894d76e6aff7005078101a06fc24ae49b1359af
achyutkadamacl <achyut.k@acldigital.com>
2023-02-03 19:17:48 +0530

    Design login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+// import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -27,10 +40,19 @@ import { UserInformationComponent } from './components/user-information/user-inf
   imports: [
     BrowserModule,
     MatCardModule,
+    MatInputModule,
     MatButtonModule,
     AppRoutingModule,
-    MatFormFieldModule,
     MatToolbarModule,
+    FormsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTableModule,
+    MatSelectModule,
+    MatOptionModule,
+    // FlexLayoutModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -6,9 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
-  username = "achyut";
-  password = "123456";
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   onButtonClick(){
-    console.log("Hello wordl");
+    // alert("Welcome! " + this.username + " " + this.password);
+    // console.log(this.username);
+    // console.log(this.password);
   }
 }
