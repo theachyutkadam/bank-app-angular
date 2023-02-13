03a1c50c4eeea9646b3ce6b5bbada10884f21b3b
achyutkadamacl <achyut.k@acldigital.com>
2023-02-13 17:49:18 +0530

    Show table with dummy record

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserInformationComponent } from './components/user-information/user-information.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   { path: 'user_information', component: UserInformationComponent },
+  { path: 'users', component: UserComponent },
   { path: '', component: SignInComponent }
 ];
 
